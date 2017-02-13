55046a1a059a7d3edd8f064212e1b1120ea4b120
andreadotti <adotti@slac.stanford.edu>
2017-02-13 11:19:22 -0800

    Generation receipes for process type
    Since number of tasks is exploding allow for receipes specific for a
    process type.

--- ProcessTest/all_macros.sh
+++ ProcessTest/all_macros.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 if [ $# -lt 1 ];then 
-    echo "usage: "$0" <physicslist>"
+    echo "usage: "$0" <physicslist> [<processName>]"
     exit 1
 fi
 pl=$1
+pname=""
+[ $# -ge 2 ]&& pname=$2
 
 host_db=/home/adotti/Work/share/G4data
 
-all_macros=`docker run -v"${host_db}:/usr/local/geant4/data:ro" --rm andreadotti/geant4-val:latest find validation -name run.mac`
+all_macros=`docker run -v"${host_db}:/usr/local/geant4/data:ro" --rm andreadotti/geant4-val:latest find validation/${pname} -name run.mac`
 
-cat <<EOF > .singleinteractions-${pl}-list.json 
- {                                                                                                                                                                              "singleinteractions-${pl}" : [
+out_file=singleinteractions-${pl}-${pname}-list.json
+
+cat <<EOF > .${out_file}
+ {
+    "singleinteractions-${pl}" : [
 EOF
 
 for macro in $all_macros;do
-        echo "      \"/runme.sh $pl $macro\"," >> .singleinteractions-${pl}-list.json
+        echo "      \"/runme.sh $pl $macro\"," >> .${out_file}
 done
-sed '$ s/.$//' .singleinteractions-${pl}-list.json > singleinteractions-${pl}-list.json
-rm .singleinteractions-${pl}-list.json
-echo "   ]" >> singleinteractions-${pl}-list.json
-echo "}" >> singleinteractions-${pl}-list.json
+sed '$ s/.$//' .${out_file}> ${out_file} 
+rm .${out_file}
+echo "   ]" >> ${out_file}
+echo "}" >> ${out_file} 
