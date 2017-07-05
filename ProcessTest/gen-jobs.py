9478567e036da458ca2ab6bb732395bded6b51b8
andreadotti <adotti@slac.stanford.edu>
2017-07-05 13:56:30 -0700

    Port to python3

--- ProcessTest/gen-jobs.py
+++ ProcessTest/gen-jobs.py
@@ -6,9 +6,9 @@ Created on Mon 23 2017
 """
 import sys
 if len(sys.argv)<2:
-    print 'usage:',sys.argv[0],'<list.json> [<outputfile.json>]'
-    print '     <list.json> is the simplified list of tasks json file'
-    print '     <outputfile.json> is the name of the generated file (default: gen-jobs.json)'
+    print('usage:',sys.argv[0],'<list.json> [<outputfile.json>]')
+    print('     <list.json> is the simplified list of tasks json file')
+    print('     <outputfile.json> is the name of the generated file (default: gen-jobs.json)')
     exit(0)
 
 templatefile='jobs-template.json'
@@ -20,14 +20,14 @@ if len(sys.argv)>2:
 import json
 import copy
 
-_f1=file(templatefile)
-_f2=file(jobsfile)
+_f1=open(templatefile,'r')
+_f2=open(jobsfile,'r')
 _template=json.load(_f1)
 _tasks_list=json.load(_f2)
 
 _job=_template[u'job_specifications'][0]
 
-_id=_tasks_list.keys()[0]
+_id=list(_tasks_list.keys())[0]
 _job[u'id']=_id
 _task=(_job[u'tasks'][0])
 _job[u'tasks']=[]
@@ -41,7 +41,7 @@ for _cmd in _tasks_list[_id]:
     _tid += 1
 
 _finalout={ u'job_specifications': [ _job ] }
-_of=file(outputfile,"w")
+_of=open(outputfile,"w")
 json.dump(_finalout,_of,indent=1)
 _of.close()
 _f1.close()
